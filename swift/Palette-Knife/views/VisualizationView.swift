f2cb27072e07bf571cbdc3bf36afb771cfa96f81
Jingyi Li <noon@berkeley.edu>
2018-03-02 12:55:58 -0800

    dots working but not erasing

--- swift/Palette-Knife/views/VisualizationView.swift
+++ swift/Palette-Knife/views/VisualizationView.swift
@@ -29,9 +29,9 @@ class VisualizationView: ModifiedCanvasView {
     
     func eraseEventHandler(data:String, key:String){
         self.removeAllStrokes();
-        print("@@ erase called");
+        print("@@ erase called, active strokes", self.activeStrokes);
         self.undoById(strokeIds: ["vstroke", "vstrokeU", "vstrokeD"])
-        print("@@ erase again");
+        print("@@ erase again active strokes ", self.activeStrokes);
 
     }
     
@@ -43,20 +43,20 @@ class VisualizationView: ModifiedCanvasView {
         
         switch(data.0){
         case "STYLUS_UP":
-            self.beginStroke(id:"vstrokeU")
-            self.renderStrokeById(currentStrokeId:"vstrokeU", toPoint:point, toWidth:5.0, toColor:UIColor.red)
+            //self.beginStroke(id:"vstrokeU")
+            self.renderStrokeById(currentStrokeId:"vstroke", toPoint:point, toWidth:5.0, toColor:UIColor.red)
             let point2 = CGPoint(x:x+1, y:y+1)
-            self.renderStrokeById(currentStrokeId:"vstrokeU", toPoint:point2, toWidth:5.0, toColor:UIColor.red)
+            self.renderStrokeById(currentStrokeId:"vstroke", toPoint:point2, toWidth:5.0, toColor:UIColor.red)
             break
         case "STYLUS_DOWN":
             //self.undoById(strokeIds: ["vstrokeStart", "vStrokeEnd"]) //undo start/end of older stroke
 
-            self.beginStroke(id:"vstrokeD")
-            self.renderStrokeById(currentStrokeId:"vstrokeD", toPoint:point, toWidth:5.0, toColor:UIColor.green)
-            let point2 = CGPoint(x:x+1, y:y+1)
-            self.renderStrokeById(currentStrokeId:"vstrokeD", toPoint:point2, toWidth:5.0, toColor:UIColor.red)
-            
             self.beginStroke(id:"vstroke")
+            self.renderStrokeById(currentStrokeId:"vstroke", toPoint:point, toWidth:5.0, toColor:UIColor.green)
+            let point2 = CGPoint(x:x+1, y:y+1)
+            self.renderStrokeById(currentStrokeId:"vstroke", toPoint:point2, toWidth:5.0, toColor:UIColor.green)
+            
+//            self.beginStroke(id:"vstroke")
             break
         case "STYLUS_MOVE":
             self.renderStrokeById(currentStrokeId:"vstroke", toPoint:point, toWidth:1.0, toColor:UIColor.cyan)
