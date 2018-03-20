0cd0ce0573d26d05aad43231c7afbd17cf81089c
Jennifer Jacobs <jacobsj@media.mit.edu>
2018-03-20 13:06:35 -0700

    changes to server.js

--- server.js
+++ server.js
@@ -51,8 +51,9 @@ wss.on('connection', (ws) => {
 	ws.on('message', function incoming(message) {
 
 		var json_data = JSON.parse(message);
-	    console.log('message', clientName, userkey, message,json_data.type=="inspector_data");
+	    console.log('message', clientName, userkey, message,json_data.type=="collection_data");
 	    //foo
+	    //
 
 
 		 if (json_data.type == "synchronize" || json_data.type == "collection_data" || json_data.type == "behavior_data" || json_data.type == "authoring_response" || json_data.type == "storage_data" || json_data.type == "inspector_data") {
