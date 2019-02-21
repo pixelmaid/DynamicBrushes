d84754894ab2c88a0c0139e302f9098719e852d0
radiolarian <noon@berkeley.edu>
2019-02-21 10:36:58 -0800

    update inspector or order but dont understand draw segment

--- javascript/app/Debugger.js
+++ javascript/app/Debugger.js
@@ -15,6 +15,7 @@ define(["app/Emitter"],
 				this.pastConstraint = null;
         this.lastTransitionId = null;
         this.inspectorInit = false;
+        this.data = null;
 				this.vizQueue = [];
                 document.onkeyup = function(e) {
                 
@@ -24,7 +25,7 @@ define(["app/Emitter"],
                        	}
                        	else{
                        		let currentConstraint = this.vizQueue.pop();
-                       		this.visualizeConstraint(currentConstraint,this.pastConstraint);
+                       		this.visualizeConstraint(currentConstraint,this.pastConstraint,this.data);
                        		this.pastConstraint = currentConstraint;
                        	}
 
@@ -32,20 +33,21 @@ define(["app/Emitter"],
                 }.bind(this);
 			}
 
-      initInspector() {
+      initInspector(data) {
         console.log("adding inspector");
         var inspectorHTML = '<div id="brush-inspector" class="w jsplumb-draggable jsplumb-droppable ui-droppable">' +
         '<ul style="padding:0 10px 0 10px;">' +
-        '<li id="param-x">position x: <span id="inspector-x-pos">x</span></li>' +
-        '<li id="param-y">position y: <span id="inspector-y-pos">x</span></li>' +
-        '<li id="param-sX">scale x: <span id="inspector-x-scale">x</span></li>' +
-        '<li id="param-sY">scale y: <span id="inspector-y-scale">x</span></li>' +
-        '<li id="param-r">rotation: <span id="inspector-rot">x</span></li>' +
-        '<li id="param-a">alpha: <span id="inspector-alpha">x</span></li>' +
-        '<li id="param-h">hue: <span id="inspector-hue">x</span></li>' +
-        '<li id="param-s">saturation: <span id="inspector-sat">x</span></li>' +
-        '<li id="param-lV">value: <span id="inspector-val">x</span></li>' +
-        '<li id="param-l">lightness: <span id="inspector-lightness">x</span></li>' +
+        '<li id="param-x">position x: <span id="inspector-x-pos">'+data.brushState.x+'</span></li>' +
+        '<li id="param-y">position y: <span id="inspector-y-pos">'+data.brushState.y+'</span></li>' +
+        '<li id="param-sX">scale x: <span id="inspector-x-scale">'+data.brushState.sX+'</span></li>' +
+        '<li id="param-sY">scale y: <span id="inspector-y-scale">'+data.brushState.sY+'</span></li>' +
+        '<li id="param-r">rotation: <span id="inspector-rot">'+data.brushState.r+'</span></li>' +
+        '<li id="param-a">alpha: <span id="inspector-alpha">'+parseFloat(data.brushState.a).toFixed(2)+'</span></li>' +
+        '<li id="param-h">hue: <span id="inspector-hue">'+data.brushState.h+'</span></li>' +
+        '<li id="param-s">saturation: <span id="inspector-sat">'+data.brushState.s+'</span></li>' +
+        '<li id="param-l">lightness: <span id="inspector-lightness">'+data.brushState.l+'</span></li>' +
+        '<li id="param-i">child index: <span id="inspector-child">'+data.brushState.i+'</span></li>' +
+        '<li id="param-lV">spawn level: <span id="inspector-spawnlv">'+data.brushState.lV+'</span></li>' +
         '</ul>' +
         '</div>'
         $('#canvas').append(inspectorHTML);
@@ -54,7 +56,7 @@ define(["app/Emitter"],
 			processInspectorData(data){
         console.log("data type is ", data.type);
         console.log("data is ", data);
-        this.updateInspector(data);
+        this.data = data;
 				switch (data.type){
                     case "DRAW_SEGMENT":
                        this.visualizeDrawSegment(data);
@@ -66,24 +68,6 @@ define(["app/Emitter"],
 
                 }
 			}
-      updateInspector(data) {
-        if (!this.inspectorInit) {
-          this.initInspector();
-          this.inspectorInit = true;
-        }
-        console.log("INSPECTOR DATA!! ", data.brushState);
-        $("#inspector-x-pos").text(data.brushState.x);
-        $("#inspector-y-pos").text(data.brushState.y);
-        $("#inspector-x-scale").text(data.brushState.sX);
-        $("#inspector-y-scale").text(data.brushState.sY);
-        $("#inspector-rot").text(data.brushState.r);
-        $("#inspector-alpha").text(parseFloat(data.brushState.a).toFixed(2));
-        $("#inspector-hue").text(data.brushState.h);
-        $("#inspector-sat").text(data.brushState.s);
-        $("#inspector-val").text(data.brushState.lV);
-        $("#inspector-lightness").text(data.brushState.l);
-
-      }
 
       displayTransition(data){
         console.log("transition via", data.transitionId);
@@ -108,7 +92,11 @@ define(["app/Emitter"],
 
 			}
 
-			visualizeConstraint(constraint,pastConstraint){
+			visualizeConstraint(constraint,pastConstraint,data){
+        if (!this.inspectorInit) {
+          this.initInspector(data);
+          this.inspectorInit = true;
+        }
         if(this.lastTransitionId) {
           $("#" + this.lastTransitionId).parent().hide();
           $("#" + this.lastTransitionId).parent().next().show(); //the toggle button
@@ -117,11 +105,49 @@ define(["app/Emitter"],
 				if(pastConstraint){
 					$("#" + pastConstraint.constraintId).removeClass("debug");
           $("#param-" + pastConstraint.name).removeClass("debug-inspect");
-          console.log("VIZ past ", pastConstraint.name);
 				} 
 				$("#" + constraint.constraintId).addClass("debug");
         $("#param-" + constraint.name).addClass("debug-inspect");
         console.log("VIZ curr ", constraint.name);
+
+        switch (constraint.name) {
+          case "x":
+              $("#inspector-x-pos").text(data.brushState.x);
+              console.log("update x");
+              break;
+          case "y":
+              $("#inspector-y-pos").text(data.brushState.y);
+                console.log("update y");
+              break;
+          case "sX":
+              $("#inspector-x-scale").text(data.brushState.sX);
+              break;
+          case "sY":
+              $("#inspector-y-scale").text(data.brushState.sY);
+              break;
+          case "r":
+              $("#inspector-rot").text(data.brushState.r);
+              break;
+          case "a":
+              $("#inspector-alpha").text(parseFloat(data.brushState.a).toFixed(2));
+              break;
+          case "h":
+              $("#inspector-hue").text(data.brushState.h);
+              break;
+          case "s":
+              $("#inspector-sat").text(data.brushState.s);
+              break;
+          case "l":
+              $("#inspector-lightness").text(data.brushState.l);
+              break;
+          case "i":
+              $("#inspector-child").text(data.brushState.i);
+              break;
+          case "lV":
+              $("#inspector-spawnlv").text(data.brushState.lV);
+              break;
+        }
+
 
         // highlight inspector console 
 
