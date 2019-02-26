72ae61a14d6b1683304cf850bdb67fa7d8731078
radiolarian <noon@berkeley.edu>
2019-02-25 19:11:43 -0800

    step through implemented

--- javascript/app/Debugger.js
+++ javascript/app/Debugger.js
@@ -23,7 +23,7 @@ define(["app/Emitter"],
 							this.trigger("STEP_FORWARD");
 						} else {
 							let currentConstraint = this.vizQueue.shift();
-							this.visualizeConstraint(currentConstraint, this.pastConstraint, this.data);
+							this.visualizeStepThrough(currentConstraint, this.pastConstraint, this.data);
 							this.pastConstraint = currentConstraint;
 						}
 
@@ -35,14 +35,14 @@ define(["app/Emitter"],
 				console.log("adding inspector");
 				var inspectorHTML = '<div id="brush-inspector" class="w jsplumb-draggable jsplumb-droppable ui-droppable">' +
 					'<ul style="padding:0 10px 0 10px;">' +
-					'<li id="param-ox">origin x: <span id="inspector-ox-pos">' + data.brushState.ox + '</span></li>' +
-					'<li id="param-oy">origin y: <span id="inspector-oy-pos">' + data.brushState.oy + '</span></li>' +
-					'<li id="param-sx">scale x: <span id="inspector-x-scale">' + data.brushState.sx + '</span></li>' +
-					'<li id="param-sy">scale y: <span id="inspector-y-scale">' + data.brushState.sy + '</span></li>' +
-					'<li id="param-rotation">rotation: <span id="inspector-rotation">' + data.brushState.rotation + '</span></li>' +
-					'<li id="param-x">position x: <span id="inspector-x-pos">' + data.brushState.x + '</span></li>' +
-					'<li id="param-y">position y: <span id="inspector-y-pos">' + data.brushState.y + '</span></li>' +
-					'<li id="param-diameter">diameter: <span id="inspector-diameter">' + data.brushState.diameter + '</span></li>' +
+					'<li id="param-ox">origin x: <span id="inspector-ox-pos">' + parseFloat(data.brushState.ox).toFixed(2) + '</span></li>' +
+					'<li id="param-oy">origin y: <span id="inspector-oy-pos">' + parseFloat(data.brushState.oy).toFixed(2) + '</span></li>' +
+					'<li id="param-sx">scale x: <span id="inspector-x-scale">' + parseFloat(data.brushState.sx).toFixed(2) + '</span></li>' +
+					'<li id="param-sy">scale y: <span id="inspector-y-scale">' + parseFloat(data.brushState.sy).toFixed(2) + '</span></li>' +
+					'<li id="param-rotation">rotation: <span id="inspector-rotation">' + parseFloat(data.brushState.rotation).toFixed(2) + '</span></li>' +
+					'<li id="param-x">position x: <span id="inspector-x-pos">' + parseFloat(data.brushState.x).toFixed(2) + '</span></li>' +
+					'<li id="param-y">position y: <span id="inspector-y-pos">' + parseFloat(data.brushState.y).toFixed(2) + '</span></li>' +
+					'<li id="param-diameter">diameter: <span id="inspector-diameter">' + parseFloat(data.brushState.diameter).toFixed(2) + '</span></li>' +
 					'<li id="param-hue">hue: <span id="inspector-hue">' + data.brushState.hue + '</span></li>' +
 					'<li id="param-saturation">saturation: <span id="inspector-saturation">' + data.brushState.saturation + '</span></li>' +
 					'<li id="param-lightness">lightness: <span id="inspector-lightness">' + data.brushState.lightness + '</span></li>' +
@@ -71,12 +71,24 @@ define(["app/Emitter"],
 			}
 
 			displayTransition(data) {
-        console.log("! displaying transition of ", data, " queue is ", this.vizQueue)
+        console.log("! displaying transition of ", data, " queue is ", this.vizQueue);
+        //add type
+        this.vizQueue.push({transitionId: data.prevState, type:"transition"});
+        this.vizQueue.push({transitionId: data.transitionId, type:"transition"});
+        for (var i = 0; i < data.methods.length; i++) {
+          data.methods[i].type = "method"
+          this.vizQueue.push(data.methods[i]);
+        }
+        var arrowObject = $("#" + data.transitionId).parent().prev();
+        this.vizQueue.push({obj: arrowObject, type: "arrow"});
+        console.log("!!!! hi????");
         // UNCOMMENT THESE LINES TO HIDE/SHOW
 				// $("#" + data.transitionId).parent().show();
 				// $("#" + data.transitionId).parent().next().hide(); //the toggle button
         // $("#" + data.transitionId).parent().next().removeClass("state active");
-        $("#" + data.transitionId).parent().addClass("state active");
+        // $("#" + data.transitionId).parent().addClass("state active");
+
+        console.log("! now queue is ", this.vizQueue);
 
 			}
 
@@ -86,7 +98,7 @@ define(["app/Emitter"],
 				$("#" + data.prevState).children(".state").removeClass("active");
 				$("#" + data.currentState).children(".state").addClass("active");
 				for (var i = 0; i < data.constraints.length; i++) {
-
+          data.constraints[i].type = "binding"
 					data.constraints[i].value = brushState[data.constraints[i].constraintId];
 					this.vizQueue.push(data.constraints[i]);
 				}
@@ -94,85 +106,107 @@ define(["app/Emitter"],
 
 			}
 
-			visualizeConstraint(constraint, pastConstraint, data) {
-        console.log("! visualizing constraints ", data);
+			visualizeStepThrough(constraint, pastConstraint, data) {
+        console.log("! visualizing constraints ", data, " queue ", this.vizQueue, " past constraint ", pastConstraint);
 				if (!this.inspectorInit) {
 					this.initInspector(data);
 					this.inspectorInit = true;
 				}
+
 				if (pastConstraint) {
-					$("#" + pastConstraint.constraintId).removeClass("debug");
-					$("#param-" + pastConstraint.relativePropertyName).removeClass("debug-inspect");
-				}
-				$("#" + constraint.constraintId).addClass("debug");
-				$("#param-" + constraint.relativePropertyName).addClass("debug-inspect");
-				console.log("VIZ curr ", constraint.relativePropertyName);
+          switch (pastConstraint.type) {
+            case "method":
+              $("#" + pastConstraint.methodId).removeClass("method-inspect");
+              break;
+            case "binding":
+              $("#" + pastConstraint.constraintId).removeClass("debug");
+              $("#param-" + pastConstraint.relativePropertyName).removeClass("debug-inspect");          
+              break;
+            case "transition":
+              if (pastConstraint.transitionId == "start") {
+                $(".setup").children().next().removeClass("start-highlight");
+              }
+              else if ($("#" + pastConstraint.transitionId).hasClass("transition_statement")) {
+                $("#" + pastConstraint.transitionId).removeClass("active");
+              } else { //it's a state
+                $("#" + pastConstraint.transitionId).children().eq(1).removeClass("active");
+              }              
+            break;            
+            case "arrow":
+              pastConstraint.obj.attr("class", "jsplumb-connector");
+              break;
+          }
+        }
 
-				switch (constraint.relativePropertyName) {
-					case "x":
-						$("#inspector-x-pos").text(data.brushState.x);
-						console.log("update x");
-						break;
-					case "y":
-						$("#inspector-y-pos").text(data.brushState.y);
-						console.log("update y");
-						break;
-					case "sx":
-						$("#inspector-x-scale").text(data.brushState.sy);
-						break;
-					case "sy":
-						$("#inspector-y-scale").text(data.brushState.sx);
-						break;
-					case "rotation":
-						$("#inspector-rotation").text(data.brushState.rotation);
-						break;
-					case "alpha":
-						$("#inspector-alpha").text(parseFloat(data.brushState.alpha).toFixed(2));
-						break;
-					case "hue":
-						$("#inspector-hue").text(data.brushState.hue);
-						break;
-					case "saturation":
-						$("#inspector-sat").text(data.brushState.saturation);
-						break;
-					case "lightness":
-						$("#inspector-lightness").text(data.brushState.lightness);
-						break;
-					case "diameter":
-						$("#inspector-diameter").text(data.brushState.diameter);
-						break;
-					case "i":
-						$("#inspector-child").text(data.brushState.i);
-						break;
-					case "lV":
-						$("#inspector-spawnlv").text(data.brushState.lV);
-						break;
-				}
+        switch (constraint.type) {
+          case "method":
+            console.log("VIZ METHOD ", constraint);
+            $("#" + constraint.methodId).addClass("method-inspect");
+            break;
+          case "binding":
+            $("#" + constraint.constraintId).addClass("debug");
+            $("#param-" + constraint.relativePropertyName).addClass("debug-inspect");
+            console.log("VIZ curr ", constraint.relativePropertyName);
 
-
-				// highlight inspector console 
-
-
-				/*   addInspector(target) {
-                var inspectorModel = new InspectorModel(this.id, target.attr("id"));
-                var el = inspectorModel.view.el;
-                target.hover(
-                    function() {
-                        var position = $(this).offset();
-                        el.css({
-                            left: position.left,
-                            top: position.top + 30,
-                            visibility: "visible"
-                        });
-                    },
-                    function() {
-
-                        el.css({
-                            visibility: "hidden"
-                        });
-
-                    });
-            }*/
+            switch (constraint.relativePropertyName) {
+              case "x":
+                $("#inspector-x-pos").text(data.brushState.x);
+                console.log("update x");
+                break;
+              case "y":
+                $("#inspector-y-pos").text(data.brushState.y);
+                console.log("update y");
+                break;
+              case "sx":
+                $("#inspector-x-scale").text(data.brushState.sy);
+                break;
+              case "sy":
+                $("#inspector-y-scale").text(data.brushState.sx);
+                break;
+              case "rotation":
+                $("#inspector-rotation").text(data.brushState.rotation);
+                break;
+              case "alpha":
+                $("#inspector-alpha").text(parseFloat(data.brushState.alpha).toFixed(2));
+                break;
+              case "hue":
+                $("#inspector-hue").text(data.brushState.hue);
+                break;
+              case "saturation":
+                $("#inspector-sat").text(data.brushState.saturation);
+                break;
+              case "lightness":
+                $("#inspector-lightness").text(data.brushState.lightness);
+                break;
+              case "diameter":
+                $("#inspector-diameter").text(data.brushState.diameter);
+                break;
+              case "i":
+                $("#inspector-child").text(data.brushState.i);
+                break;
+              case "lV":
+                $("#inspector-spawnlv").text(data.brushState.lV);
+                break;
+            }         
+            break;
+          case "transition":
+            console.log("! VIZ TRANSITION ", constraint, pastConstraint);
+            if (constraint.transitionId == "start") {
+              $(".setup").children().next().addClass("start-highlight");
+            }
+            if ($("#" + constraint.transitionId).hasClass("transition_statement")) {
+              $("#" + constraint.transitionId).addClass("active");
+            } else { //it's a state
+              $("#" + constraint.transitionId).children().eq(1).addClass("active");
+            }
+            break;            
+          case "arrow":
+            console.log("! VIZ TRANSITION (arrow)");
+            constraint.obj.attr("class", "jsplumb-connector jsplumb-hover debug-inspect");
+            console.log("! OBJ IS", constraint.obj);
+            break;
+        }
+				
 			}
 
 		};
